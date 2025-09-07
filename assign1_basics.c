73e7586fdd1a3431c2fc7d961e567498ea3b6463
mayafrncis <maya_francis@edu.aua.am>
2025-09-07 16:56:40 +0400

    Edited Assignment 1

--- assign1_basics.c
+++ assign1_basics.c
@@ -4,4 +4,7 @@ int var = 4;
 int* p = &var;
 printf("Address of var using var itself: %p\n", &var);
 printf("Address of var using its pointer: %p\n", p);
+printf("Value of var: %d\n", var);
+*p = 5;
+printf("New Value of var: %d\n", var);
 }
