f4d137a36ee075db9a510a611ab19360be3101b8
mayafrncis <maya_francis@edu.aua.am>
2025-09-07 17:16:57 +0400

    Assignment 2

---
+++ assign2_arithmetic.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+int arr[] = {1,2,3,4,5};
+int* p = arr;
+printf("Printing the values of the array using pointers:\n");
+for(int i = 0; i < 5; i++) {
+    printf("%d\n", *(p + i));
+}
+printf("Replacing the 2nd element with 10.\n");
+*(arr+1) = 10;
+printf("Replacing the 5th element with 0.\n");
+*(arr+4) = 0;
+p = arr;
+printf("Printing the values of the array using pointers:\n");
+for(int i = 0; i < 5; i++) {
+    printf("%d\n", *(p + i));
+}
+printf("Printing the values of the array using array name:\n");
+for(int i = 0; i < 5; i++) {
+    printf("%d\n", arr[i]);
+}
+}
